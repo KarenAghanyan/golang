9219e5894c47e8ed2e2409fdf6b5fc18a1755f79
Karen <78427845+KarenAghanyan@users.noreply.github.com>
2021-10-21 02:02:06 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,56 +1,72 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"log"
 	"net/http"
-	"simpleAPI/routs"
+	"project/models"
+	"project/routs"
+	"strings"
 
-	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "karen"
-	password = "1111"
-	dbname   = "postgres"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
 
-	router := mux.NewRouter()
-	psqlconn := fmt.Sprintf("host = %s port = %d user = %s password = %s dbname =%s sslmode = disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlconn)
+	conn, err := connectDB()
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 
-	defer db.Close()
+	router := gin.Default()
 
-	sqlStatement := `insert into "Users"("id", "firstname", "lastname", 
-	 									"username", "email", "password_hushed")
-	 									values($1, $2, $3, $4, $5, $6)
-	 									RETURNING id`
-	id := 0
-	err = db.QueryRow(sqlStatement, 30, "jon@calhoun.io", "Jonathan", "Calhoun").Scan(&id)
-	if err != nil {
-		panic(err)
+	router.Use(dbMiddleware(*conn))
+
+	usersGroup := router.Group("users")
+	{
+		usersGroup.POST("register", routs.UsersRegister)
+		usersGroup.POST("login", routs.UsersLogin)
 	}
 
-	router.HandleFunc("/register", routs.UserRegister).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8000", router))
+	router.Run(":3000")
 }
 
-// Request sample
-// {
-//     "firstname": "Karen",
-//     "lastname": "Aghanyan",
-//     "username": "Karenus",
-//     "email": "karen@gmail.com",
-//     "password": "112358",
-//     "password_confirm": "112358"
-// }
+func connectDB() (c *pgx.Conn, err error) {
+	conn, err := pgx.Connect(context.Background(), "postgres://karen:1111@localhost:5432/mydb?sslmode=verify-ca")
+	if err != nil || conn == nil {
+		fmt.Println("Error connecting to DB")
+		fmt.Println(err.Error())
+	}
+	_ = conn.Ping(context.Background())
+	return conn, err
+}
+
+func dbMiddleware(conn pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("postgres", conn)
+		c.Next()
+	}
+}
+
+func authMiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		bearer := c.Request.Header.Get("Authorization")
+		split := strings.Split(bearer, "Bearer ")
+		if len(split) < 2 {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authenticated."})
+			c.Abort()
+			return
+		}
+		token := split[1]
+		//fmt.Printf("Bearer (%v) \n", token)
+		isValid, userID := models.IsTokenValid(token)
+		if isValid == false {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authenticated."})
+			c.Abort()
+		} else {
+			c.Set("user_id", userID)
+			c.Next()
+		}
+	}
+}
